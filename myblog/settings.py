38395d2b98a730cba54ae8d277079cfa2936afa4
dillip singh <dk381759@gmail.com>
2021-10-09 15:50:14 +0545

    final commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -131,10 +131,10 @@ AUTH_USER_MODEL = 'account.Account'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'media'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
